43c8b8eb76e3c7a624bf791111c2b8fe07e152a5
Danton Noriega <danton.noriega@gmail.com>
2016-06-24 19:28:28 -0400

    tests edits. CHECK passing locally but Travis build failing...

--- tests/testthat/test-map-gtfs.R
+++ tests/testthat/test-map-gtfs.R
@@ -16,16 +16,41 @@ check_url <- function() {
   }
 }
 
+
+url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
+gtfs_obj <- url %>% import_gtfs(quiet=TRUE)
+not_gtfs_obj <- 123
+
 # gtfs_map_*()
 test_that('Mapping single stops and routes', {
 
-	url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
-	gtfs_obj <- url %>% import_gtfs(quiet=TRUE)
-	not_gtfs_obj <- 123
 
 	yes_stop_id <- "778070"
 	not_stop_id1 <- 12345
 	not_stop_id2 <- "SUPBRO"
+
+	yes_route_id <- "1693"
+	not_route_id1 <- 12345
+	not_route_id2 <- "SUPBRO"
+
+	# map_gtfs_stop()
+	expect_is(map_gtfs_stop(gtfs_obj, yes_stop_id), 'leaflet')
+	expect_error(map_gtfs_stop(gtfs_obj, stop_id = not_stop_id1))
+	expect_error(map_gtfs_stop(gtfs_obj, stop_id = not_stop_id2))
+	expect_error(map_gtfs_stop(not_gtfs_obj, stop_id = not_stop_id1))
+	expect_error(map_gtfs_stop(not_gtfs_obj, stop_id = not_stop_id2))
+
+	# map_gtfs_route_stops()
+	expect_is(map_gtfs_route_stops(gtfs_obj, yes_route_id), 'leaflet')
+	expect_error(map_gtfs_route_stops(gtfs_obj, not_route_id1))
+	expect_error(map_gtfs_route_stops(gtfs_obj, not_route_id2))
+	expect_error(map_gtfs_route_stops(not_gtfs_obj, not_route_id1))
+	expect_error(map_gtfs_route_stops(not_gtfs_obj, not_route_id2))
+
+})
+
+# gtfs_map_*()
+test_that('Mapping one or more routes or full network', {
 
 	yes_route_id <- "1693"
 	yes_route_ids <- c("1693", "1689")
@@ -37,41 +62,28 @@ test_that('Mapping single stops and routes', {
 	no_service_ids <- c("SUPBRO", 11111111)
 	ok_service_ids <- c("SUPBRO", "1505A4978")
 
-	# map_gtfs_stop()
-	expect_is(map_gtfs_stop(gtfs_obj, yes_stop_id), 'leaflet') # leaflet check
-	expect_error(map_gtfs_stop(gtfs_obj, stop_id = not_stop_id1))
-	expect_error(map_gtfs_stop(gtfs_obj, stop_id = not_stop_id2))
-	expect_error(map_gtfs_stop(not_gtfs_obj, stop_id = not_stop_id1))
-	expect_error(map_gtfs_stop(not_gtfs_obj, stop_id = not_stop_id2))
-
-	# map_gtfs_route_stops()
-	expect_is(map_gtfs_route_stops(gtfs_obj, yes_route_id), 'leaflet') # leaflet check
-	expect_error(map_gtfs_route_stops(gtfs_obj, not_route_id1))
-	expect_error(map_gtfs_route_stops(gtfs_obj, not_route_id2))
-	expect_error(map_gtfs_route_stops(not_gtfs_obj, not_route_id1))
-	expect_error(map_gtfs_route_stops(not_gtfs_obj, not_route_id2))
 
 	# map_gtfs_route_shape()
-	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_id), 'leaflet') # leaflet check
-	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_ids), 'leaflet') # leaflet check
-	expect_warning(map_gtfs_route_shape(gtfs_obj, ok_route_ids)) # leaflet check
-	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_ids, yes_service_ids), 'leaflet') # leaflet check
-	expect_warning(map_gtfs_route_shape(gtfs_obj, yes_route_ids, ok_service_ids)) # leaflet check
-	expect_error(map_gtfs_route_shape(gtfs_obj, yes_route_ids, no_service_ids)) # leaflet check
+	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_id), 'leaflet')
+	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_ids), 'leaflet')
+	expect_is(map_gtfs_route_shape(gtfs_obj, yes_route_ids, yes_service_ids), 'leaflet')
+	expect_warning(map_gtfs_route_shape(gtfs_obj, ok_route_ids))
+	expect_warning(map_gtfs_route_shape(gtfs_obj, yes_route_ids, ok_service_ids))
+	expect_error(map_gtfs_route_shape(gtfs_obj, yes_route_ids, no_service_ids))
 	expect_error(map_gtfs_route_shape(gtfs_obj, not_route_id1))
 	expect_error(map_gtfs_route_shape(gtfs_obj, not_route_id2))
 	expect_error(map_gtfs_route_shape(not_gtfs_obj, not_route_id1))
 	expect_error(map_gtfs_route_shape(not_gtfs_obj, not_route_id2))
 
 	# map_gtfs_agency_routes()
-	expect_is(map_gtfs_agency_routes(gtfs_obj, agency_id = 240), 'leaflet') # leaflet check
+	expect_is(map_gtfs_agency_routes(gtfs_obj, agency_id = 240), 'leaflet')
 	expect_error(map_gtfs_agency_routes(gtfs_obj, agency_id = not_route_id1))
 	expect_error(map_gtfs_agency_routes(gtfs_obj, agency_id = not_route_id2))
 	expect_error(map_gtfs_agency_routes(not_gtfs_obj, agency_id = not_route_id1))
 	expect_error(map_gtfs_agency_routes(not_gtfs_obj, agency_id = not_route_id2))
 
 	# add stops should still return leaflet object
-	expect_is(map_gtfs_agency_routes(gtfs_obj, agency_id = 240, TRUE), 'leaflet') # leaflet check
+	expect_is(map_gtfs_agency_routes(gtfs_obj, agency_id = 240, TRUE), 'leaflet')
 	expect_error(map_gtfs_agency_routes(gtfs_obj, agency_id = 240, "9"))
 
 })
