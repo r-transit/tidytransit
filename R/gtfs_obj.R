4d7fe2bae03cd4eb2d7c4e4d75b5b4e4ceba88e4
Tom B <buckley.tom@Gmail.com>
2018-08-05 00:22:35 -0700

    Delete gtfs_obj.R

--- R/gtfs_obj.R
+++
@@ -1,8 +0,0 @@
-#' Example GTFS data
-#' 
-#' Data obtained from 
-#' \url{http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip}. 
-#' @name gtfs_obj
-#' @docType data
-#' @seealso convert_gtfs_routes_to_sf
-NULL 
