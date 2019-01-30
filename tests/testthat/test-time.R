b26edbecabe9180581bf8b3dba8ef9f247098b99
Tom Buckley <buckley.tom@gmail.com>
2019-01-30 00:00:09 -0800

    more comments out for hms/frequencies

--- tests/testthat/test-time.R
+++ tests/testthat/test-time.R
@@ -12,10 +12,10 @@ test_that('set_hms_times() works with valid data', {
   gtest$stop_times_df <- dplyr::tibble(
     arrival_time = c("08:00:00", "14:00:00", "26:10:00"),
     departure_time = c("08:00:10", "14:00:20", "26:10:30"))
-  gtest$frequencies_df = dplyr::tibble(
-    start_time = c("06:00:00"),
-    end_time = c("12:00:00")
-  )
+  # gtest$frequencies_df = dplyr::tibble(
+  #   start_time = c("06:00:00"),
+  #   end_time = c("12:00:00")
+  # )
 
   gtest <- tidytransit:::set_hms_times(gtest)  
   
@@ -26,10 +26,10 @@ test_that('set_hms_times() works with valid data', {
   expect_false(is.na(gtest$stop_times_df$arrival_time_hms[3]))
   expect_equal(gtest$stop_times_df$departure_time_hms[3], hms::hms(26*3600+10*60+30))
   
-  expect_is(gtest$frequencies_df$start_time_hms, "hms")
-  expect_is(gtest$frequencies_df$end_time_hms, "hms")
-  expect_is(gtest$frequencies_df$start_time, "character")
-  expect_is(gtest$frequencies_df$end_time, "character")
+  # expect_is(gtest$frequencies_df$start_time_hms, "hms")
+  # expect_is(gtest$frequencies_df$end_time_hms, "hms")
+  # expect_is(gtest$frequencies_df$start_time, "character")
+  # expect_is(gtest$frequencies_df$end_time, "character")
 })
 
 test_that('get_date_service_table() uses the right dates', { 
