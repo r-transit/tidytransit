c154b84f7b8531220b73aef080e9924c26fd4bb8
Flavio Poletti <flavio.poletti@hotmail.ch>
2019-02-18 18:11:41 +0100

    add _sf suffix for stops again
    while removing it in the vignette since filtering an sf object doesn't seem to work

--- R/spatial.R
+++ R/spatial.R
@@ -6,7 +6,7 @@
 #' @export
 gtfs_as_sf <- function(gtfs_obj, quiet) {
   if(!quiet) message('Converting stops to simple features ')
-  gtfs_obj$stops <- try(stops_df_as_sf(gtfs_obj$stops))
+  gtfs_obj$stops_sf <- try(stops_df_as_sf(gtfs_obj$stops))
   if(!quiet) message('Converting routes to simple features ')
   gtfs_obj$routes_sf <- try(routes_df_as_sf(gtfs_obj))
   return(gtfs_obj)
