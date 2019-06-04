ff7f09d6524c285546d0f44cedc0953d6a5eb79f
Flavio Poletti <flavio.poletti@hotmail.ch>
2019-06-04 19:40:59 +0200

    remove has_bom()

--- R/import.R
+++ R/import.R
@@ -112,37 +112,6 @@ download_from_url <- function(url,
   return(path)
 }
 
-#' Checks UTF-8-BOM encoding.
-#' 
-#' Special thanks to @patperu for finding the issue and 
-#' to @hrbrmstr for the code to help deal with the issue.
-#' 
-#' @param path the path the the text file
-#' @param encoding can be one of \code{UTF-8}, \code{UTF-16} or \code{UTF-16BE}.
-#'        Although a BOM could be used with UTF-32 and other encodings, such
-#'        encodings are rarely used for data transmission and the three supported
-#'        encodings are the most likely ones folks in R will be working with from
-#'        web APIs.\cr\cr
-#'        This function defaults to looking for \code{UTF-8} BOM, but you can
-#'        override it.
-#' @return \code{TRUE} if response contains a BOM, \code{NA} if an unsupported encoding
-#'         was passed (along with a message)
-#' @references \href{http://www.unicode.org/faq/utf_bom.html}{UTF-8, UTF-16, UTF-32 & BOM}
-#' @noRd
-#' @keywords internal
-#' @author @@hrbrmstr
-
-has_bom <- function(path, encoding="UTF-8") {
-
-  B <- readBin(path, "raw", 4, 1)
-  switch(encoding,
-       `UTF-8`=B[1]==as.raw(0xef) & B[2]==as.raw(0xbb) & B[3]==as.raw(0xbf),
-       `UTF-16`=B[1]==as.raw(0xff) & B[2]==as.raw(0xfe),
-       `UTF-16BE`=B[1]==as.raw(0xfe) & B[2]==as.raw(0xff),
-       { message("Unsupported encoding") ; return(NA) }
-  )
-}
-
 #' Unzip a file and delete zip
 #'
 #' @param zipfile path to zipped file
@@ -354,20 +323,19 @@ parse_gtfs_file <- function(prefix, file_path, quiet = FALSE) {
         "D" = readr::col_date(format = "%Y%m%d")
       )
     
-      df <- suppressWarnings(
-        readr::read_csv(file = file_path, 
-          col_types = coltypes
-        )
+    df <- suppressWarnings(
+      readr::read_csv(file = file_path, 
+                      col_types = coltypes
       )
-      probs <- readr::problems(df)
-      
-      if(dim(probs)[1] > 0) {
-        attributes(df) <- append(attributes(df), list(problems = probs))
-        warning(paste0("Parsing failures while reading ", prefix))
-        print(probs)
-      }
-
+    )
+    probs <- readr::problems(df)
+    
+    if(dim(probs)[1] > 0) {
+      attributes(df) <- append(attributes(df), list(problems = probs))
+      warning(paste0("Parsing failures while reading ", prefix))
+      print(probs)
+    }
+    
     return(df)
   } else return(NULL)
-
 }
