8102f8d985c8dfa7a09ae9938ff9661b6f485575
Flavio Poletti <flavio.poletti@hotmail.ch>
2020-11-30 10:58:06 +0100

    remove travis

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,29 +1,26 @@
 context("Utils")
 
-if(Sys.getenv("USER") != "travis") {
-  test_that("write_gtfs creates the same feed as read by read_gtfs", {
-    skip_on_cran()
-    path1 <- system.file("extdata", "sample-feed-fixed.zip", package = "tidytransit")
-    path2 <- tempfile(fileext = ".zip")
-    g1 <- read_gtfs(path1)
-    g1_hms <- set_hms_times(g1)
-    write_gtfs(g1_hms, path2)
-    g2 <- read_gtfs(path2)
-    expect_equal(g1, g2)
-    expect_error(write_gtfs(g1$agency, path2))
-  })
-}
+test_that("write_gtfs creates the same feed as read by read_gtfs", {
+  skip_on_cran()
+  path1 <- system.file("extdata", "sample-feed-fixed.zip", package = "tidytransit")
+  path2 <- tempfile(fileext = ".zip")
+  g1 <- read_gtfs(path1)
+  g1_hms <- set_hms_times(g1)
+  write_gtfs(g1_hms, path2)
+  g2 <- read_gtfs(path2)
+  expect_equal(g1, g2)
+  expect_error(write_gtfs(g1$agency, path2))
+})
 
-if(Sys.getenv("USER") != "travis") {
-  test_that("write_gtfs as_dir", {
-    skip_on_cran()
-    path1 <- system.file("extdata", "sample-feed-fixed.zip", package = "tidytransit")
-    path2 <- tempdir()
-    g1 <- read_gtfs(path1)
-    write_gtfs(g1, path2, as_dir = TRUE)
-    expect_true(file.exists(paste0(path2, "/agency.txt")))
-  })
-}
+test_that("write_gtfs as_dir", {
+  skip_on_cran()
+  path1 <- system.file("extdata", "sample-feed-fixed.zip", package = "tidytransit")
+  path2 <- tempdir()
+  g1 <- read_gtfs(path1)
+  write_gtfs(g1, path2, as_dir = TRUE)
+  expect_true(file.exists(paste0(path2, "/agency.txt")))
+})
+
 
 test_that("summary.gtfs", {
   gpath <- system.file("extdata", "routing.zip", package = "tidytransit")
