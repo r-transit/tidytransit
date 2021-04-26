0321e47151f728d52b179525303df0c581331dda
Flavio Poletti <flavio.poletti@hotmail.ch>
2021-04-26 11:40:30 +0200

    update gtfs_duke dataset to new format

--- data-raw/create-gtfs-obj.R
+++ data-raw/create-gtfs-obj.R
@@ -1,5 +1,5 @@
 devtools::load_all()
 url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
-gtfs_duke <- read_gtfs(url, quiet=TRUE)
+gtfs_duke <- read_gtfs(url)
 saveRDS(gtfs_duke, here::here('data-raw/gtfs_duke'))
 usethis::use_data(gtfs_duke)
