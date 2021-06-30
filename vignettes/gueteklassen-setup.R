5820fd3084c0a0f023fd12c141becd47cc341746
Flavio Poletti <flavio.poletti@hotmail.ch>
2021-06-30 10:29:18 +0200

    kanton zug

--- vignettes/gueteklassen-setup.R
+++ vignettes/gueteklassen-setup.R
@@ -3,14 +3,7 @@ library(tidytransit)
 
 gtfs_full = read_gtfs("~/Downloads/gtfs_fp2021_2021-06-16_09-10.zip")
 
-# bbox = osmdata::getbb("Zug, Switzerland", featuretype = "city")
-bbox = c(8.4738273, 47.0810323, 8.5775926, 47.1898484)
+bbox = c(8.3906, 47.0988, 8.5668, 47.2246)
 g <- filter_feed_by_area(gtfs_full, bbox)
 
-write_gtfs(g, "inst/extdata/gtfs_ch_zug.zip")
-
-# 
-# g$stops %>%
-#   filter(stop_in_area) %>%
-#   mapview::mapview()
-
+write_gtfs(g, "inst/extdata/gtfs_ch_zg.zip")
