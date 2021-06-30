c0e9d5dcdb2f0aaa829e16786732338283f7b93f
Flavio Poletti <flavio.poletti@hotmail.ch>
2021-06-30 11:27:39 +0200

    filter stop times //wip

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,16 @@ feed_contains <- function(gtfs_obj, table_name) {
   exists(table_name, where = gtfs_obj) ||
     (exists(".", where = gtfs_obj) && exists(table_name, where = gtfs_obj$.))
 }
+
+# stops_table = g$stops
+# 
+# create_stop_name_tbl = function(stop_table) {
+#   
+#   
+#   parent_station_ids = stops_table$parent_station
+# 
+#   parent_stops = stops_table %>% filter(stop_id %in% parent_station_ids)
+#   parent_stops
+#   
+#   gtfs_obj$stops
+# }
--- vignettes/gueteklassen-setup.R
+++ vignettes/gueteklassen-setup.R
@@ -3,7 +3,7 @@ library(tidytransit)
 
 gtfs_full = read_gtfs("~/Downloads/gtfs_fp2021_2021-06-16_09-10.zip")
 
-bbox = c(8.3906, 47.0988, 8.5668, 47.2246)
+bbox = c(8.44546, 47.16418, 8.55613, 47.20850)
 g <- filter_feed_by_area(gtfs_full, bbox)
 
 write_gtfs(g, "inst/extdata/gtfs_ch_zg.zip")
