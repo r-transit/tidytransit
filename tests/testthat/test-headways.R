344cf533045826c4540ad5bd4cdb0a22355c20f3
Flavio Poletti <flavio.poletti@hotmail.ch>
2021-11-18 11:32:17 +0100

    change by_route default to TRUE to match doc

--- tests/testthat/test-headways.R
+++ tests/testthat/test-headways.R
@@ -2,8 +2,8 @@ context("Frequencies are calculated correctly")
 
 # TODO rewrite with synthesized sample data
 test_that("Stop frequencies (headways) for included data are as expected", {
-  expect_equal(nrow(get_stop_frequency(gtfs_duke)), 47)
-  expect_equal(nrow(get_stop_frequency(gtfs_duke, start_hour = 10, end_hour = 11)), 41)
+  expect_equal(nrow(get_stop_frequency(gtfs_duke, by_route = FALSE)), 47)
+  expect_equal(nrow(get_stop_frequency(gtfs_duke, start_hour = 10, end_hour = 11, by_route = FALSE)), 41)
 
   stops_frequency <- get_stop_frequency(gtfs_duke, service_ids = "c_853_b_19828_d_64")
   ex_address <- stops_frequency$mean_headway[stops_frequency$stop_id==778058]
