6ee8b24ad4c9da246cab0d53eea3cbe5ea574024
Flavio Poletti <flavio.poletti@hotmail.ch>
2021-11-18 11:32:29 +0100

    use start/end_time instead of start/end_hour

--- R/frequencies.R
+++ R/frequencies.R
@@ -2,15 +2,18 @@
 #' 
 #' Note that some GTFS feeds contain a frequency data frame already. 
 #' Consider using this instead, as it will be more accurate than what 
-#' tidytransit calculates. 
+#' tidytransit calculates.
 #' 
 #' @param gtfs_obj a list of gtfs dataframes as read by [read_gtfs()].
-#' @param start_hour (optional) an integer indicating the start hour (default 6)
-#' @param end_hour (optional) an integer indicating the end hour (default 22)
-#' @param service_ids (optional) a set of service_ids from the calendar dataframe 
+#' @param start_time analysis start time, can be given as "HH:MM:SS", 
+#'                   hms object or numeric value in seconds.
+#' @param end_time analysis perdiod end time, can be given as "HH:MM:SS", 
+#'                 hms object or numeric value in seconds.
+#' @param service_ids A set of service_ids from the calendar dataframe 
 #'                    identifying a particular service id. If not provided the service_id 
-#'                    with the most departures is used
-#' @param by_route default TRUE, if FALSE then calculate headway for any line coming through the stop in the same direction on the same schedule. 
+#'                    with the most departures is used.
+#' @param by_route Default TRUE, if FALSE then calculate headway for any line coming 
+#'                 through the stop in the same direction on the same schedule. 
 #' @return dataframe of stops with the number of departures and the headway 
 #'         (departures divided by timespan) as columns.
 #'
@@ -24,11 +27,16 @@
 #' x <- order(stop_frequency$mean_headway)
 #' head(stop_frequency[x,])
 get_stop_frequency <- function(gtfs_obj,
-                               start_hour = 6,
-                               end_hour = 22,
+                               start_time = "06:00:00",
+                               end_time = "22:00:00",
                                service_ids = NULL,
-                               by_route = FALSE) {
+                               by_route = TRUE) {
   n_deps <- direction_id <- NULL
+  
+  if(is.character(start_time)) start_time <- hhmmss_to_seconds(start_time)
+  if(is.character(end_time)) end_time <- hhmmss_to_seconds(end_time)
+  
+  # get service id with most departures
   if(is.null(service_ids)) {
     dep_per_trip = gtfs_obj$stop_times %>% 
       dplyr::group_by(trip_id) %>% dplyr::count(name = "n_deps") %>% 
@@ -39,27 +47,29 @@ get_stop_frequency <- function(gtfs_obj,
       dplyr::arrange(dplyr::desc(n_deps))
     service_ids = dep_per_service_id$service_id[1]  
   }
+  
+  # filter stop_times to service_ids and start/end_time
   trips = gtfs_obj$trips %>% filter(service_id %in% service_ids)
   
-  # TODO change times to hms or strings
   stop_times = gtfs_obj$stop_times %>%
     filter(trip_id %in% trips$trip_id) %>% 
-    filter(departure_time >= start_hour*3600 & arrival_time <= end_hour*3600) %>% 
+    filter(departure_time >= start_time & arrival_time <= end_time) %>% 
     left_join(trips[c("trip_id", "route_id", "direction_id", "service_id")], "trip_id") 
   
-  # find number of departure per stop_id, route_id, service_id
+  # find number of departure per stop_id (route_id, direction_id, service_id)
   if(by_route) {
-  freq = stop_times %>% 
-    dplyr::group_by(stop_id, route_id, direction_id, service_id) %>% 
-    dplyr::count(name = "n_departures") %>% dplyr::ungroup()
+    freq = stop_times %>% 
+      dplyr::group_by(stop_id, route_id, direction_id, service_id) %>% 
+      dplyr::count(name = "n_departures") %>% dplyr::ungroup()
   } else {
     freq = stop_times %>% 
       dplyr::group_by(stop_id, service_id) %>% 
       dplyr::count(name = "n_departures") %>% dplyr::ungroup()
   }
+  
   # calculate average headway
-  duration = (end_hour-start_hour)*3600
+  duration = as.numeric(end_time-start_time)
   freq$mean_headway <- round(duration / freq$n_departures)
-
+  
   freq
 }
