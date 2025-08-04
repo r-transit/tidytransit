94b49baaed785be6d558359700a380e932f43dae
Flavio Poletti <flavio.poletti@hotmail.ch>
2025-08-04 13:51:16 +0200

    remove empty optional tables from gtfs_duke
    areas, rider_categories, linked_datasets, fare_rider_categories, farezone_attributes, timetables, timetable_stop_order

--- data-raw/load-gtfs_duke.R
+++ data-raw/load-gtfs_duke.R
@@ -3,8 +3,10 @@ url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
 gtfs_duke <- read_gtfs(url)
 usethis::use_data(gtfs_duke)
 
-gtfs_duke[1:24] <- gtfs_duke[1:24] |> 
-  lapply(\(x) {
-    attr(x, ".internal.selfref") <- NULL
-    x
-  })
+for(tblname in setdiff(names(gtfs_duke), c(".", "calendar_dates", "frequencies"))) {
+  if(nrow(gtfs_duke[[tblname]]) == 0) {
+    gtfs_duke[[tblname]] <- NULL
+  }
+}
+
+usethis::use_data(gtfs_duke, overwrite = T)
