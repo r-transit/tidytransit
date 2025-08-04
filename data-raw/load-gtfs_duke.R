33263024bd7e979788e98206caf1e05fa937b950
Flavio Poletti <flavio.poletti@hotmail.ch>
2025-08-04 13:19:26 +0200

    remove .internal.selfref attributes for gtfs_duke

--- data-raw/load-gtfs_duke.R
+++ data-raw/load-gtfs_duke.R
@@ -2,3 +2,9 @@ devtools::load_all()
 url <- "http://data.trilliumtransit.com/gtfs/duke-nc-us/duke-nc-us.zip"
 gtfs_duke <- read_gtfs(url)
 usethis::use_data(gtfs_duke)
+
+gtfs_duke[1:24] <- gtfs_duke[1:24] |> 
+  lapply(\(x) {
+    attr(x, ".internal.selfref") <- NULL
+    x
+  })
